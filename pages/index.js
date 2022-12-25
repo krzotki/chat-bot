229c92f6f20aedb03ce700817dd94a93e78c30f7
Karol Rzotki <krzoti@gmail.com>
2022-12-25 13:52:40 +0100

    bugfix for code display

--- pages/index.js
+++ pages/index.js
@@ -26,6 +26,7 @@ SyntaxHighlighter.registerLanguage("python", python);
 
 const CodeBlock = {
   code({ node, inline, className, children, ...props }) {
+    const match = /language-(\w+)/.exec(className || "");
     return !inline && match ? (
       <SyntaxHighlighter
         style={vscDarkPlus}
@@ -97,7 +98,7 @@ export default function Home() {
             <ReactMarkdown
               rehypePlugins={[rehypeRaw, rehypeKatex]}
               remarkPlugins={[remarkMath]}
-              components={{ CodeBlock }}
+              components={CodeBlock}
             >
               {message}
             </ReactMarkdown>
